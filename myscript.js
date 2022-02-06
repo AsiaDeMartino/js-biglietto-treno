61d1a7e553b1ed424c6317ba8a891807981c0142
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-06 20:37:52 +0100

    aggiunto controllo numeri

--- myscript.js
+++ myscript.js
@@ -4,7 +4,21 @@ console.log(numeroKilometri);
 //chiedere età
 const età = parseInt(prompt("Quanti anni hai?"));
 console.log(età);
+
+if( isNaN(numeroKilometri)) {
+    alert("devi inserire un numero!")
+}
+
+if( isNaN(età)) {
+    alert("devi inserire un numero!")
+}
+
+// if( (isNaN(numeroKilometri)) || (isNaN(numeroKilometri)) ) {
+//     alert("Devi inserire un numero!")
+// }
+
 //calcolare costo viaggio (km * 0,21€)
+
 //applicare sconto 20% se l'età è > 18
 //applicare sconto 40% se l'età è > 65
 //aggiungere prezzo all'html
