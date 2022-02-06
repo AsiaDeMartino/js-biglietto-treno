09bcdb0cecb0b48a2e988fbabfca88bd30130f90
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-06 20:19:49 +0100

    schema di base

---
+++ myscript.js
@@ -0,0 +1,6 @@
+//chiedere numero kilometri
+//chiedere età
+//calcolare costo viaggio (km * 0,21€)
+//applicare sconto 20% se l'età è > 18
+//applicare sconto 40% se l'età è > 65
+//aggiungere prezzo all'html
