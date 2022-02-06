9664ad71bbafa08aa012fd422e85c78347e5442f
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-06 20:26:58 +0100

    aggiunte costanti kilometri e età

--- myscript.js
+++ myscript.js
@@ -1,5 +1,9 @@
 //chiedere numero kilometri
+const numeroKilometri = parseInt(prompt("Quanti kilometri devi fare?"));
+console.log(numeroKilometri);
 //chiedere età
+const età = parseInt(prompt("Quanti anni hai?"));
+console.log(età);
 //calcolare costo viaggio (km * 0,21€)
 //applicare sconto 20% se l'età è > 18
 //applicare sconto 40% se l'età è > 65
