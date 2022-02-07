bb63b4d12a5274c898ee8167dd2b97e5c230cd82
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-07 15:05:13 +0100

    correzione

--- myscript.js
+++ myscript.js
@@ -1,36 +1,31 @@
 //chiedere numero kilometri
-const numeroKilometri = parseInt(prompt("Quanti kilometri devi fare?"));
+let numeroKilometri = parseFloat(prompt("Quanti kilometri devi fare?"));
 console.log(numeroKilometri);
 //chiedere età
-const età = parseInt(prompt("Quanti anni hai?"));
+let età = parseInt(prompt("Quanti anni hai?"));
 console.log(età);
 
-if( isNaN(numeroKilometri)) {
-    alert("devi inserire un numero!");
-}
-
-if( isNaN(età)) {
-    alert("devi inserire un numero!");
-}
-
-// if( (isNaN(numeroKilometri)) || (isNaN(numeroKilometri)) ) {
-//     alert("Devi inserire un numero!")
+// if( isNaN(numeroKilometri)) {
+//     alert("devi inserire un numero!");
 // }
 
-//calcolare costo viaggio (km * 0,21€)
+// if( isNaN(età)) {
+//     alert("devi inserire un numero!");
+// }
+
+if( isNaN(numeroKilometri) || isNaN(età) ) {
+     alert("Devi inserire un numero!")
+} else{
+    //calcolare costo viaggio (km * 0,21€)
 let costo = numeroKilometri * 0.21;
 console.log(costo);
 
-//applicare sconto 20% se l'età è > 18
-if( età <= 18 ) {
+if( età < 18 ) {                          //applicare sconto 20% se l'età è > 18
     sconto = (costo * 20) / 100;
     console.log(sconto);
     costo = costo - sconto;
     console.log(costo);
-}
-
-//applicare sconto 40% se l'età è > 65
-else if( età >= 65 ) {
+} else if( età > 65 ) {                    //applicare sconto 40% se l'età è > 65
     sconto = (costo * 40) / 100;
     console.log(sconto);
     costo = costo - sconto;
@@ -39,3 +34,6 @@ else if( età >= 65 ) {
 
 //aggiungere prezzo all'html
 document.getElementById("ticket").innerHTML += (costo) + "€";
+}
+
+
